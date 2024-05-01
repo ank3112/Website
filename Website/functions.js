c58412374b9ca86302b069d42906d31b08b05a72
Ankita <155298762+ank3112@users.noreply.github.com>
2024-05-01 20:11:15 +0530

    Add files via upload

---
+++ Website/functions.js
@@ -0,0 +1,3 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("arr").innerHTML = fruits;
+document.getElementById("demo").innerHTML = fruits.copyWithin(2,0);
---
+++ Website/subjects.js
@@ -0,0 +1,76 @@
+function drop(s)
+{
+var a = document.getElementById("sel");
+if (sel.value == "BA")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br>Hindi<br>English<br>Political Science<br>History<br>Economics</p>";
+}
+if (a.value == "BAeng")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br>English literature<br>Twentieth Century Indian Writing<br>Forms of Popular Fiction<br>Classical Literature<br>Contemporary Literature<br>Anglo-American writing from 1930<br>Women's Writing of the Nineteenth and Twentieth Centuries<br>Modern European Drama<br>Concurrent - Discipline Centered </p>";
+}
+if (a.value == "BCom")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br>Financial accounting<br>Business organisation & management<br>Business mathematics<br>Business economics<br>Principles of marketing<br>Business statistics<br>Corporate accounting<br>Income tax law & practice<br>Advertising<br>Business law<br>Foreign trade<br>Cost accounting<br>Computerized accounting system<br>E-commerce<br>Management accounting<br>Human resource management<br>Indian financial system<br>GST<br>Auditing<br>Rural marketing</p>";
+}
+if (a.value == "BSc NM")
+{
+b.innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "BSc M")
+{
+b.innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "BCA")
+{
+b.innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "BAMC")
+{
+b.innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "BPEd")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "MApol.sc")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "MApsycho")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "MAeng")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "MAhist")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "MAeco")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "MCom")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "MSc")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "MCA")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "PGDMC")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+if (a.value == "PGDCA")
+{
+document.getElementById("print").innerHTML="<p>Books name - <br> </p>";
+}
+}
